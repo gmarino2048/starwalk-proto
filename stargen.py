d8fb52024d029055988e80bda3bcdfef7a834dac
Guy Marino <gxm262@case.edu>
2019-10-14 18:34:45 -0400

    Implement star class

--- stargen.py
+++ stargen.py
@@ -1,4 +1,23 @@
 
+from math import sqrt
+
+class Star:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
+
+    def x(self):
+        return self.x
+
+    def y(self):
+        return self.y
+
+    def dist(self, other: Star):
+        x_squared = (self.x - other.x) ^ 2
+        y_squared = (self.y - other.y) ^ 2
+
+        return sqrt(x_squared + y_squared)
+
 
 def generate_stars(n: int):
     pass
