7d18a30087ab0e50e6d1d05a94d0c15172da1a0e
Guy Marino <gxm262@case.edu>
2019-10-14 18:56:09 -0400

    Add star generation function

--- stargen.py
+++ stargen.py
@@ -1,15 +1,16 @@
 
-from math import sqrt
+from random import random
+from math import sqrt, floor
 
 class Star:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
-    def x(self):
+    def get_x(self):
         return self.x
 
-    def y(self):
+    def get_y(self):
         return self.y
 
     def dist(self, other: Star):
@@ -19,5 +20,20 @@ class Star:
         return sqrt(x_squared + y_squared)
 
 
-def generate_stars(n: int):
-    pass
+def generate_stars(n: int, limits: (int, int), floor: bool = False) -> [Star]:
+    star_list = []
+
+    for _ in range(n):
+        # TODO: Make the random function pseudorandom
+        x = random() * limits[0]
+        y = random() * limits[1]
+
+        if floor:
+            x = floor(x)
+            y = floor(y)
+        
+        star = Star(x, y)
+        star_list.append(star)
+
+    return star_list
+
